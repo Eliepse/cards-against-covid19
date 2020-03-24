e46f034d4fbe859adfd7c6a30fdc98985d3cf24d
Eliepse <contact@eliepse.fr>
2020-03-24 13:39:43 +0100

    front: better home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+	<div class="container h-screen m-auto flex items-center justify-center">
+		<div class="w-full max-w-xs">
+			<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+				<a class="bg-blue-500 hover:bg-blue-700 text-white font-bold text-center py-3 px-4 rounded
+						focus:outline-none focus:shadow-outline block my-4"
+				   href="{{ action([\App\Http\Controllers\CardController::class, 'create']) }}">Créateur de cartes</a>
+				<a class="bg-blue-500 hover:bg-blue-700 text-white font-bold text-center py-3 px-4 rounded
+						focus:outline-none focus:shadow-outline block my-4"
+				   href="{{ action([\App\Http\Controllers\CardController::class, 'index']) }}">Liste des cartes</a>
+			</div>
+		</div>
+	</div>
 @endsection
