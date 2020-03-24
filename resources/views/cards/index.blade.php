18bb3fab02dff7db482f0e78568cff4974305de2
Eliepse <contact@eliepse.fr>
2020-03-24 14:32:01 +0100

    front: correct card background

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -11,7 +11,7 @@
 		<main class="container mx-auto flex flex-row text-lg flex-wrap py-10">
 
 			@foreach($deck as $card)
-				<div class="@if($card->isAnswer()) bg-gray-100 text-gray-900 @else bg-gray-900 text-gray-100 @endif
+				<div class="@if($card->isAnswer()) bg-white text-gray-900 @else bg-gray-900 text-gray-100 @endif
 						w-56 h-64 m-4 border border-gray-300 rounded-lg p-5 flex flex-col justify-between shadow-lg">
 					<p>{{ $card->text }}</p>
 					@if($card->contributor)
