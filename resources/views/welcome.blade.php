e6b478d42f83a6319df3675f4fa9b1c1ef119f8e
Eliepse <contact@eliepse.fr>
2020-03-25 13:49:45 +0100

    front: change welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,44 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="text-gray-900 antialiased leading-tight">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<title>{{ config('app.name', 'Cards Against COVID-19') }}</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+	<!-- Scripts -->
+	<script src="{{ asset('js/app.js') }}" defer></script>
 
-            .full-height {
-                height: 100vh;
-            }
+	<!-- Styles -->
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="min-h-screen bg-gray-100">
+<div class="fixed top-0 left-0 right-0 flex justify-between bg-white px-8 py-4">
+	<span class="font-black text-blue-700">CAC</span>
+	@auth
+		<ul>
+			<li><a class="text-gray-700 hover:text-gray-900 underline" href="/home">Home</a></li>
+		</ul>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+		<form method="POST" action="{{ action([\App\Http\Controllers\Auth\LoginController::class, 'logout']) }}">
+			@csrf
+			<button type="submit" class="text-sm uppercase text-gray-700 hover:text-gray-900">Logout</button>
+		</form>f
+	@endauth
+	@guest
+		<div>
+			<a class="text-sm uppercase text-gray-700 hover:text-gray-900 mx-6" href="{{ route('login') }}">Login</a>
+			<a class="text-sm uppercase text-gray-700 hover:text-gray-900" href="{{ route('login') }}">Register</a>
+		</div>
+	@endguest
+</div>
+<div id="app" class="min-h-screen pt-12 flex items-center justify-center flex-col">
+	<h1 class="text-4xl mb-4">Cards Against COVID-19</h1>
+	<p class="">Un jeu confiné par Eliepse et agrémenté par tous !</p>
+</div>
+</body>
 </html>
+
