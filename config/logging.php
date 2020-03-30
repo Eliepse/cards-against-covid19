ade3a7fab4facd81c7a6dfa9c362ba6d68c29646
Eliepse <contact@eliepse.fr>
2020-03-30 09:31:57 +0200

    fix: log level and default

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', 'daily'),
 
     /*
     |--------------------------------------------------------------------------
@@ -50,7 +50,7 @@ return [
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
+            'level' => env('LOG_LEVEL', 'warning'),
             'days' => 14,
         ],
 
