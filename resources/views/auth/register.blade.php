af48d7a62d030726d860b14dc3e1d420881e0731
Elie Meignan <contact@eliepse.fr>
2020-04-08 17:27:31 +0200

    fix: remove useless code

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,15 +38,3 @@
 <a class="btn btn--black card__btn--register" href="{{ route('login') }}">J'ai déjà un compte</a>
 </body>
 </html>
-
-@section('content')
-	<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('register') }}">
-		@csrf
-		<div class="flex items-center justify-between">
-			<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
-						focus:outline-none focus:shadow-outline" type="submit">
-				{{ __('Register') }}
-			</button>
-		</div>
-	</form>
-@endsection
